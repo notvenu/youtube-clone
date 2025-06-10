dda1570d735b70fb21cbbf54a1b2abc6c5e9fe95
notvenu <venu.kasibhatla@gmail.com>
2025-06-10 17:45:44 +0530

    feat: implement user and video models with JWT authentication and pagination support

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, // Cloundinary Url
+        required: true,
+    },
+    thumbnail: {
+        type: String, // Cloundinary Url
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number, //From Cloudniary
+        required: true,
+    },
+    views: {
+        types: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: false,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model('Video', videoSchema)
